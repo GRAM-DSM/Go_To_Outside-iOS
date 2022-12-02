fa3750857c7f00f09babcf46b1530ffe11dcfcf7
BJCHO0501 <bjcho1503@naver.com>
2022-12-02 15:22:03 +0900

    ⚙️ :: 프로젝트 생성

---
+++ GoToOutside/GoToOutside/Source/Application/GoToOutsideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoToOutsideApp.swift
+//  GoToOutside
+//
+//  Created by 조병진 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct GoToOutsideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoToOutside/GoToOutside/Source/Scene/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GoToOutside
+//
+//  Created by 조병진 on 2022/12/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
