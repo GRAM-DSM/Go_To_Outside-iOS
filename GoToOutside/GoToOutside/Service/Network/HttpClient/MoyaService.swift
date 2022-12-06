1fd7d34fc0929991229a2de2a0e83243fedc4987
BJCHO0501 <bjcho1503@naver.com>
2022-12-06 16:56:00 +0900

    📝 :: MoyaService

---
+++ GoToOutside/GoToOutside/Service/Network/HttpClient/MoyaService.swift
@@ -0,0 +1,46 @@
+import Foundation
+import Moya
+
+enum HttpService {
+    
+    //Auth
+    case login(
+        accountID: String,
+        password: String
+    )
+    case studentSignup(
+        accountID: String,
+        password: String,
+        number: String,
+        name: String
+    )
+    case teacherSignup(
+        accountID: String,
+        password: String,
+        name: String,
+        grade: String,
+        group: String,
+        code: String
+    )
+    
+    //pass
+    case requestGoToOutside(
+        reason: String,
+        outingTime: String
+    )
+    case getOutsideList
+    case getOutsideDetail(outsideID: Int)
+    case allowGoToOutside(
+        incomingTime: String,
+        outsideID: Int
+    )
+    
+}
+
+extension HttpService: TargetType {
+    var baseURL: URL { httpBaceURL()}
+    var path: String { httpPath() }
+    var method: Moya.Method { httpMethod() }
+    var task: Moya.Task { httpTask() }
+    var headers: [String : String]? { httpHeader() }
+}
