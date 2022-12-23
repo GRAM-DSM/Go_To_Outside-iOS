f7f0a9b88d8a66132a1fdea5af4af00bb85229fb
BJCHO0501 <bjcho1503@naver.com>
2022-12-24 08:55:02 +0900

    📝 :: Colors

--- GoToOutside/GoToOutside/Resource/Colors.swift
+++ GoToOutside/GoToOutside/Resource/Colors.swift
@@ -4,7 +4,7 @@ extension Color {
     static let gray0 = Color("gray 0")
     static let gray1 = Color("gray 1")
     static let gray2 = Color("gray 2")
+    static let gray3 = Color("gray 3")
     static let green0 = Color("green")
     static let red0 = Color("red")
-    static let signUpSelector = Color("SignUpSelector")
 }
