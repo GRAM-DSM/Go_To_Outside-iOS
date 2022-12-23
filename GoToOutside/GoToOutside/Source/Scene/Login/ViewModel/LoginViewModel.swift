6d9c63a17166a6adb1a962efb53feb2e19df536a
BJCHO0501 <bjcho1503@naver.com>
2022-12-24 08:54:07 +0900

    📝 :: LoginViewModel

--- GoToOutside/GoToOutside/Source/Scene/Login/ViewModel/LoginViewModel.swift
+++ GoToOutside/GoToOutside/Source/Scene/Login/ViewModel/LoginViewModel.swift
@@ -1,6 +1,25 @@
 import Foundation
+import Moya
 
 class LoginViewModel: ObservableObject {
+    var httpService = MoyaProvider<HttpService>()
     @Published var accountID = ""
     @Published var password = ""
+    @Published var isSuccess = false
+    func login() {
+        httpService.request(.login(accountID: accountID, password: password)) { res in
+            switch res {
+            case .success(let result):
+                switch result.statusCode {
+                case 200:
+                    print("로그인 성공")
+                    self.isSuccess = true
+                default:
+                    print(result.statusCode)
+                }
+            case .failure(let err):
+                print("login err\(err.localizedDescription)")
+            }
+        }
+    }
 }
