094b85a3334fac88c014148128e09df73e60835e
BJCHO0501 <bjcho1503@naver.com>
2022-12-24 13:17:58 +0900

    📝 :: TeacherSignUpPWView

--- GoToOutside/GoToOutside/Source/Scene/SignUp/View/Teacher/TeacherSignUpPWView.swift
+++ GoToOutside/GoToOutside/Source/Scene/SignUp/View/Teacher/TeacherSignUpPWView.swift
@@ -1,14 +1,14 @@
 //
-//  SignUpView.swift
+//  TeacherSignUpPWView.swift
 //  GoToOutside
 //
-//  Created by 홍승재 on 2022/12/06.
+//  Created by 조병진 on 2022/12/24.
 //
 
 import SwiftUI
 
-struct SignUpPWView: View {
-    @StateObject var signupViewModel = SignupViewModel()
+struct TeacherSignUpPWView: View {
+    @StateObject var signUpViewModel = SignUpViewModel()
     var body: some View {
         ZStack {
             VStack(alignment: .leading, spacing: 0) {
@@ -20,21 +20,21 @@ struct SignUpPWView: View {
                 Text("비밀번호 입력")
                     .foregroundColor(.black)
                     .font(.custom("NotoSansKR-Bold", size: 40))
-                    .padding(.bottom, 71)
+                    .padding(.bottom, 50)
                 Group {
                     VStack(spacing: 17) {
                         TextFieldView(
-                            text: $signupViewModel.accountPassword,
+                            text: $signUpViewModel.accountPassword,
                             placeholder: "비밀번호",
-                            font: .custom("NotoSansKR-Light", size: 18),
                             indicator: "20글자 이상의 비밀번호로 입력해주세요 :)",
+                            font: .custom("NotoSansKR-Light", size: 18),
                             isSecurity: true
                         )
                         TextFieldView(
-                            text: $signupViewModel.accountPassword,
+                            text: $signUpViewModel.accountPassword,
                             placeholder: "비밀번호 확인",
-                            font: .custom("NotoSansKR-Light", size: 18),
                             indicator: "위의 비밀번호와 동일한 비밀번호를 입력해 주세요!",
+                            font: .custom("NotoSansKR-Light", size: 18),
                             isSecurity: true
                         )
                     }
@@ -43,7 +43,7 @@ struct SignUpPWView: View {
                 NavigationLink(destination: EmptyView()) {
                     HStack {
                         Spacer()
-                        Text("회원가입")
+                        Text("계속하기")
                             .foregroundColor(.white)
                             .font(.custom("NotoSansKR-Bold", size: 20))
                             .padding(.vertical, 14)
@@ -60,8 +60,8 @@ struct SignUpPWView: View {
     }
 }
 
-struct SignUpPWView_Previews: PreviewProvider {
+struct TeacherSignUpPWView_Previews: PreviewProvider {
     static var previews: some View {
-        SignUpPWView()
+        TeacherSignUpPWView()
     }
 }
